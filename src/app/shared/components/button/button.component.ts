29d33b1ea2433adccee3af5cba881395d900b4d7
elifintizamoglu <elifintizam@gmail.com>
2024-07-13 23:51:24 +0300

    Button Component Added

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {
+
+  // State
+
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() variant: ButtonVariant = 'primary';
+  @Output() click = new EventEmitter<MouseEvent>();
+
+  //Lifecycle
+
+  // Main
+
+  // Events
+  onClick(event: MouseEvent) {
+    this.click.emit(event);
+  }
+
+  // Helpers
+  get buttonClass(): string {
+    return `btn btn-${this.variant}`;
+  }
+}
+
+export type ButtonVariant =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'light'
+  | 'dark'
+  | 'link';
