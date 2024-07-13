c39a96ad21f790c97bee0d647b8bd323e819d89f
elifintizamoglu <elifintizam@gmail.com>
2024-07-13 23:51:58 +0300

    Navbar Added

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, TemplateRef } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '../button/button.component';
+
+export type NavItem = {
+  label: string;
+  link: string;
+};
+
+export type NavTitle = {
+  text: string;
+  routerLink?: string | string[];
+} | undefined;
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    ButtonComponent
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+
+export class NavbarComponent {
+
+  @Input() navItems: NavItem[] = [];
+  @Input() title: NavTitle;
+  @Input() endContentTemplate?: TemplateRef<any>;
+
+  constructor() { }
+
+  isUrl(url: string): boolean {
+    return url.startsWith('http') || url.startsWith('https');
+  }
+
+  // navigateToProfile() {
+  //   if (this.tokenService.isAdmin()) {
+  //     this.router.navigate(['/management/brands']);
+  //   } else {
+  //     this.router.navigate(['/user']);
+  //   }
+  // }
+}
