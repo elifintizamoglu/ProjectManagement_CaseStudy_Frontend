a4dac7826c09a8f3974c14a474399bd22fbfd658
elifintizamoglu <elifintizam@gmail.com>
2024-07-13 23:52:23 +0300

    Home-layout Added

--- src/app/features/projects/project-list/project-list.component.ts
+++ src/app/features/projects/project-list/project-list.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { GetAllProjectsResponse, ProjectsControllerService } from '../../../shared/services/api';
+import { HomeLayoutComponent } from '../../../shared/layouts/home-layout/home-layout.component';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
   imports: [
     CommonModule,
+    HomeLayoutComponent,
   ],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.scss',
---
+++ src/app/shared/layouts/home-layout/home-layout.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavbarComponent, NavItem, NavTitle } from '../../components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-home-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    FooterComponent,
+    ButtonComponent,
+    RouterModule,
+  ],
+  templateUrl: './home-layout.component.html',
+  styleUrl: './home-layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeLayoutComponent { 
+  
+  navTitle: NavTitle = { text: 'Project Management', routerLink: '/' };
+
+  navItems: NavItem[] = [
+    { label: 'Projects', link: '/projects' },
+  ];
+}
